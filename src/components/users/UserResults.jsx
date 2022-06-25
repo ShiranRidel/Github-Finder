28ee573b5ce9014ff2eddd78f5ed5fdf14c72d5d
ShiranRidel <86361159+ShiranRidel@users.noreply.github.com>
2022-06-25 19:39:18 +0300

    remove </React.StrictMode> to render ones

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -8,7 +8,7 @@ function UserResults() {
   const fetchUsers = async () => {
     const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
       headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        Authorization: `basic +${ process.env.REACT_APP_GITHUB_TOKEN}`,
       },
     });
     const data = await response.json();
