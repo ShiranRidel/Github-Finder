0742e01351f248366fc3bc25a2598dea6c3a976d
ShiranRidel <86361159+ShiranRidel@users.noreply.github.com>
2022-06-27 00:19:21 +0300

    update function fetchUser to searchUsers & the params & update UserSearch component to work and search

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,21 +1,22 @@
-import { useState , useContext} from "react";
+import { useState, useContext } from "react";
 import GithubContext from "../../context/github/GithubContext";
 
-
 function UserSearch() {
-    const [text,setText]=useState('')
-    const {users}= useContext(GithubContext)
-    const handleChange =(e)=>setText(e.target.value)
-    const handleSubmit =(e)=>{
-        e.preventDefault()
-        if(text===''){
-            alert('Please enter something')
-        }else{
-            //@todo - search users
-            setText('')
-        }
+  const [text, setText] = useState("");
 
+  const { users, searchUsers } = useContext(GithubContext);
+
+  const handleChange = (e) => setText(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text === "") {
+      alert("Please enter something");
+    } else {
+      searchUsers(text)
+      setText("");
     }
+  };
 
   return (
     <div
@@ -47,10 +48,9 @@ function UserSearch() {
       </div>
       {users.length > 0 && (
         <div>
-        <button className="btn btn-ghost btn-lg">Clear</button>
-      </div>
+          <button className="btn btn-ghost btn-lg">Clear</button>
+        </div>
       )}
-      
     </div>
   );
 }
