d3593b519115d3db6a60ae8ea5901b07f392f3b0
ShiranRidel <86361159+ShiranRidel@users.noreply.github.com>
2022-06-26 21:47:39 +0300

    delete useEffect &create case:'SET_LOADING'

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -7,16 +7,15 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
-  //   const [users, setUsers] = useState([]);
-  //   const [loading, setLoading] = useState(true);
-
   const initialState = {
     users: [],
-    loading: true,
+    loading: false,
   };
-  const [state,dispatch] = useReducer(githubReducer, initialState);
+  const [state, dispatch] = useReducer(githubReducer, initialState);
 
+  //Get initial users (testing purposes)
   const fetchUsers = async () => {
+    setLoading();
     const response = await fetch(`${GITHUB_URL}/users`, {
       headers: {
         Authorization: `basic +${GITHUB_TOKEN}`,
@@ -24,15 +23,19 @@ export const GithubProvider = ({ children }) => {
     });
     const data = await response.json();
     dispatch({
-        type: 'GET_USERS',
-        payload: data
-    })
+      type: "GET_USERS",
+      payload: data,
+    });
   };
+
+  //set loading
+  const setLoading = () => dispatch({ type: "SET_LOADING" });
+
   return (
     <GithubContext.Provider
       value={{
-        users:state.users,
-        loading:state.loading,
+        users: state.users,
+        loading: state.loading,
         fetchUsers,
       }}
     >
