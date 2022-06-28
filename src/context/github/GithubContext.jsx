c92ad84ede49b44fc5515afc731d785771c3519f
ShiranRidel <86361159+ShiranRidel@users.noreply.github.com>
2022-06-28 23:51:42 +0300

    get User latest repos

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -10,6 +10,7 @@ export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
   };
   const [state, dispatch] = useReducer(githubReducer, initialState);
@@ -17,10 +18,10 @@ export const GithubProvider = ({ children }) => {
   //Get search results
   const searchUsers = async (text) => {
     setLoading();
-  const params = new URLSearchParams({
+    const params = new URLSearchParams({
       q: text,
     });
-  const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `basic +${GITHUB_TOKEN}`,
       },
@@ -36,15 +37,15 @@ export const GithubProvider = ({ children }) => {
   //Get singel user
   const getUser = async (login) => {
     setLoading();
-  
-  const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
       headers: {
         Authorization: `basic +${GITHUB_TOKEN}`,
       },
     });
-    if(response.status === 404){
-      window.location = '/notfound'
-    } else{
+    if (response.status === 404) {
+      window.location = "/notfound";
+    } else {
       const data = await response.json();
 
       dispatch({
@@ -52,14 +53,36 @@ export const GithubProvider = ({ children }) => {
         payload: data,
       });
     }
-    
+  };
+  //Get latest user repos
+  const getUserRepos = async (login) => {
+    setLoading();
+    const params = new URLSearchParams({
+      sort: "created",
+      per_page: 10,
+    });
+
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos?${params}`,
+      {
+        headers: {
+          Authorization: `basic +${GITHUB_TOKEN}`,
+        },
+      }
+    );
+    const data = await response.json();
+
+    dispatch({
+      type: "GET_REPOS",
+      payload: data,
+    });
   };
 
   //set loading
   const setLoading = () => dispatch({ type: "SET_LOADING" });
 
   //clear users from state
-  const clearUsers = ()=> dispatch({type:"CLEAR_USERS"})
+  const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
 
   return (
     <GithubContext.Provider
@@ -67,9 +90,11 @@ export const GithubProvider = ({ children }) => {
         users: state.users,
         loading: state.loading,
         user: state.user,
+        repos: state.repos,
         searchUsers,
         clearUsers,
-        getUser
+        getUser,
+        getUserRepos,
       }}
     >
       {children}
